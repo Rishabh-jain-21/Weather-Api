d8d51355715a8be1476bf3402e58bb07676077dc
Rishabh <nonu47596@gmail.com>
2021-11-08 01:23:56 +0530

    completed news api

---
+++ src/components/Temp.js
@@ -0,0 +1,71 @@
+// api.openweathermap.org/data/2.5/weather?q=pune&appid=abd793a49182af06d9e646d3b0ab6d1c
+import React, { useEffect, useState } from "react";
+import "./style.css";
+import WeatherCard from "./WeatherCard";
+const Temp = () => {
+  const [inputText, setInputText] = useState("karnal");
+  const [tempData, setTempData] = useState({});
+  //setting input feild
+  const setInput = (e) => {
+    setInputText(e.target.value);
+  };
+
+  // API called and data recieved
+  const getWeatherData = async () => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${inputText}&units=metric&appid=abd793a49182af06d9e646d3b0ab6d1c`;
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+
+      // object destructuring in api
+      const { temp, humidity, pressure } = data.main;
+      const { main: weathermood } = data.weather[0];
+      const { name } = data;
+      const { speed } = data.wind;
+      const { country, sunset } = data.sys;
+
+      // taking all objects to use in app
+      const myWeatherInfo = {
+        temp,
+        humidity,
+        pressure,
+        weathermood,
+        name,
+        speed,
+        country,
+        sunset,
+      };
+      setTempData(myWeatherInfo);
+      console.log(tempData);
+
+      //   console.log(temperature);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // on the time first time this fxn need to be called
+  useEffect(() => {
+    getWeatherData();
+  }, []);
+
+  return (
+    <div className="container center">
+      <div className="container-1 ">
+        <div className="input-Container center">
+          <input
+            type="text"
+            placeholder="Enter Country or City"
+            value={inputText}
+            onChange={setInput}
+          />
+          <button onClick={getWeatherData}>Search</button>
+        </div>
+        <WeatherCard tempData={tempData} />
+      </div>
+    </div>
+  );
+};
+
+export default Temp;
---
+++ src/components/WeatherCard.js
@@ -0,0 +1,113 @@
+import React, { useState, useEffect } from "react";
+
+const WeatherCard = (props) => {
+  const [weatherIcon, setweatherIcon] = useState("sunny");
+  const {
+    temp,
+    humidity,
+    pressure,
+    weathermood,
+    name,
+    speed,
+    country,
+    sunset,
+  } = props.tempData;
+
+  useEffect(() => {
+    if (weathermood) {
+      switch (weathermood) {
+        case "Clouds":
+          setweatherIcon("wi-day-cloudy");
+          break;
+        case "Haze":
+          setweatherIcon("wi-fog");
+          break;
+        case "Clear":
+          setweatherIcon("wi-day-sunny");
+          break;
+        case "Rain":
+          setweatherIcon("wi-showers");
+          break;
+        case "Snow":
+          setweatherIcon("wi-snow");
+          break;
+        default:
+          setweatherIcon("wi-day-sunny");
+          break;
+      }
+    }
+  }, [weathermood]);
+
+  //converting susets seconds into time
+  let sec = sunset;
+  let date = new Date(sec * 1000);
+  let timeStr = `${date.getHours()}:${date.getMinutes()}`;
+
+  return (
+    <div className="Display-container">
+      {console.log(props)}
+      <div className="first center">
+        <i style={{ fontSize: 60 }} className={`wi ${weatherIcon}`}></i>
+      </div>
+      <div className="second">
+        <div
+          className="second-child-1 center"
+          style={{
+            justifyContent: "space-between",
+            padding: "0px 70px 0px 10px",
+          }}
+        >
+          <span style={{ color: "white", fontSize: "60px" }}>{temp}°</span>
+          <div
+            style={{
+              color: "white",
+              fontSize: "30px",
+            }}
+          >
+            {weathermood}{" "}
+            <div>
+              {name} , {country}
+            </div>
+          </div>
+        </div>
+        <div className="second-child-2 center">
+          {new Date().toLocaleString()}
+        </div>
+      </div>
+      <div className="third">
+        <div className="width">
+          <i className="wi wi-sunset third-text"></i>
+          <span className="third-text">
+            <p>Sunset at</p>
+            <p>{timeStr}</p>
+          </span>
+        </div>
+        <div className="width">
+          <i className="wi wi-humidity third-text"></i>
+          <span className="third-text">
+            {" "}
+            <p>{humidity}</p>
+            <p>Humidity</p>
+          </span>
+        </div>
+        <div className="width">
+          <i className="wi wi-hail third-text"></i>
+          <span className="third-text">
+            {" "}
+            <p>Pressure</p>
+            <p>{pressure}</p>
+          </span>
+        </div>
+        <div className="width">
+          <i className="wi wi-strong-wind third-text"></i>
+          <span className="third-text">
+            {" "}
+            <p>{speed}</p>
+            <p>Speed</p>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default WeatherCard;
